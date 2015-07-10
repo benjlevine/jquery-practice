c7b774a5d4d4ca550550addfa79bee8426f7ec44
benjlevine <ben@point5.com>
2015-07-09 20:10:08 -0400

    uncommented functions

--- js/exercise.js
+++ js/exercise.js
@@ -13,22 +13,22 @@
  * We've started you off, replace the sec
  */
 
-//	function question1(){
-//		$("p").css("color", "blue");
-//	}
+	function question1(){
+		$("p").css("color", "blue");
+	}
 
-//	$("#button1").click(question1);
+	$("#button1").click(question1);
 
 /**
  * Question 2:
  * When button 1 is clicked, select all <h2> elements on this page, and change the text to say your name.
  */
 
-// function question2(){
-//   $("h2").html("Benjamin Levine!");
-// 	}
+function question2(){
+  $("h2").html("Benjamin Levine!");
+	}
 
-// $("#button1").click(question2);
+$("#button1").click(question2);
 
 /**
  * Question 3:
@@ -36,11 +36,11 @@
  * Using jQuery change the content to read "True Fact".
  */
 
-// function question3(){
-// 	$("ol li.falseFact").html("True Fact");
-// }
+function question3(){
+	$("ol li.falseFact").html("True Fact");
+}
 
-// $("#button1").click(question3);
+$("#button1").click(question3);
 
 
 //SECTION 2: Now you are on your own. Write the necessary code to accomplish the following.
@@ -49,33 +49,33 @@
  * When button 2 is clicked, change the background color of the whole page to "pink"
  */
 
-// function question4(){
-//   $('body, header').css('background-color', 'pink');
-// }
+function question4(){
+  $('body, header').css('background-color', 'pink');
+}
 
-// $('#button2').click(question4);
+$('#button2').click(question4);
 
 /**
  * Question 5:
  * When button 2 is clicked, change the color of all h2's to "green"
  */
 
-// function question5(){
-//   $('h2').css('color', 'green');
-// }
+function question5(){
+  $('h2').css('color', 'green');
+}
 
-// $('#button2').click(question5);
+$('#button2').click(question5);
 
 /**
  * Question 6:
  * When button 2 is clicked, change the html content of all "blockquotes" to be "<span>no quote</span>"
  */
 
-// function question6(){
-//   $('blockquote').html('<span>no quote</span>');
-// }
+function question6(){
+  $('blockquote').html('<span>no quote</span>');
+}
 
-// $('#button2').click(question6);
+$('#button2').click(question6);
 
 //SECTION 3: This section is more difficult. Try your hand at the next 3 questions.
 
@@ -84,11 +84,11 @@
  * When button 3 is clicked, change the text in the header's h1 tag to read "jQuery Ninja"
  */
 
-// function question7(){
-//   $('header h1').html('jQuery Ninja');
-// }
+function question7(){
+  $('header h1').html('jQuery Ninja');
+}
 
-// $('#button3').click(question7);
+$('#button3').click(question7);
 
 /**
  * Question 8:
@@ -97,11 +97,11 @@
  */
 
 // changes the photo from a building to some veggies
-// function question8(){
-//   $(".city").attr("src", "http://lorempixel.com/g/500/400/food");
-// }
+function question8(){
+  $(".city").attr("src", "http://lorempixel.com/g/500/400/food");
+}
 
-// $('#button3').click(question8);
+$('#button3').click(question8);
 
 /**
  * Question 9:
@@ -109,8 +109,8 @@
  * Bonus: Try to do this with only one jQuery call. $("selector").css({attribute: "value", attribute: "value"});
  */
 
-// function question9(){
-//   $('.wrapper p').css({'color': 'blue', 'font-family': 'georgia'});
-// }
+function question9(){
+  $('.wrapper p').css({'color': 'blue', 'font-family': 'georgia'});
+}
 
-// $('#button3').click(question9);
+$('#button3').click(question9);
